b17e996381c330f66fe32e9cc67d1006cb811891
hicoderindia <hicoder.dolly@gmail.com>
2021-11-27 14:27:04 +0530

    updated

--- src/Components/Project/Sidebar/Sidebar.js
+++ src/Components/Project/Sidebar/Sidebar.js
@@ -52,19 +52,19 @@ const Sidebar = () => {
       {/* Marketing */}
       <div className="sidebar--name">
         <OnlinePredictionRoundedIcon className="sidebar--name--icon2" />
-        <h4>Marketing</h4>
+        <div>Marketing</div>
       </div>
 
       {/* Announcement */}
       <div className="sidebar--name">
         <CampaignRoundedIcon className="sidebar--name--icon2" />
-        <h4>Announcement</h4>
+        <div>Announcement</div>
       </div>
 
       {/* Announcement */}
       <div className="sidebar--name">
         <ConnectWithoutContactIcon className="sidebar--name--icon2" />
-        <h4>Direct Message</h4>
+        <div>Direct Message</div>
       </div>
     </div>
   );
