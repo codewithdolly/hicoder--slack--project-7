173fd234704f797aba4bb92c687396f1a1efc6be
hicoderindia <hicoder.dolly@gmail.com>
2021-11-30 09:44:56 +0530

    updated

--- src/Components/Project/Chats/Chats.js
+++ src/Components/Project/Chats/Chats.js
@@ -1,10 +1,17 @@
+import { Button } from '@mui/material'
+import "./Chats.scss"
 import React from 'react'
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 
 const Chats = () => {
     return (
-        <div>
-            Hi
-        </div>
+        <>
+           <div className="chats">
+           {/* <div></div> */}
+           <Button className="chats--date" >Tuesday, November 25th</Button>
+
+           </div> 
+        </>
     )
 }
 
