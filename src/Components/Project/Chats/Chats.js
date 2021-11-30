54f7c136b958e1aa48f8ef09ebf03299cc2c76ff
hicoderindia <hicoder.dolly@gmail.com>
2021-11-30 10:16:49 +0530

    updated

--- src/Components/Project/Chats/Chats.js
+++ src/Components/Project/Chats/Chats.js
@@ -2,13 +2,21 @@ import { Button } from '@mui/material'
 import "./Chats.scss"
 import React from 'react'
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import Shubh from "../Images/subh.jpg"
 
 const Chats = () => {
     return (
         <>
            <div className="chats">
-           {/* <div></div> */}
-           <Button className="chats--date" >Tuesday, November 25th</Button>
+           <Button className="chats--date" >Tuesday, November 25th <KeyboardArrowDownOutlinedIcon color="action" /></Button>
+           <div className="chats--singleChat">
+           <div className="chats--singleChat--img">
+                <img src={Shubh} alt="Naga"  className="chats--singleChat--imge"/></div>
+                <div  className="chats--singleChat--details">
+                  <p>Subh <small>07:15 AM</small></p>
+                  <p><span>@channel</span> Hello Guys, Today we got one new big project. So congratulation & cheers!</p>
+                </div>
+           </div>
 
            </div> 
         </>
